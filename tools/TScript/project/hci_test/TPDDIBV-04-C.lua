7d8184111f082f0f8e14ad9458ae3c830117ace3
yafei.tu <yafei.tu@telink-semi.com>
2017-01-03 09:37:34 +0800

    hci TPDSUBV-05-C update;TPDDIBV-04-C fix

--- tools/TScript/project/hci_test/TPDDIBV-04-C.lua
+++ tools/TScript/project/hci_test/TPDDIBV-04-C.lua
@@ -5,7 +5,7 @@ handle_bulk = tl_usb_bulk_monitor_init(0xffff);
 --- print("handle : ",string.format("0x%04x",handle_bulk)) 
 tl_usb_bulk_monitor_start(handle_bulk)
 
-hci_le_set_scan_enbale(0,BLE_SUCCESS)
+hci_le_set_scan_enable(0,BLE_SUCCESS)
 
 local answer
 io.write("continue with this operation (y/n)? ")
--- tools/TScript/project/hci_test/hci_le_create_conn.lua
+++ tools/TScript/project/hci_test/hci_le_create_conn.lua
@@ -7,15 +7,13 @@ arg = {...}
 hci_type_cmd = HCI_TYPE_CMD
 opcode_OCF = HCI_CMD_LE_CREATE_CONNECTION
 opcode_OGF = HCI_CMD_LE_OPCODE_OGF
-cmd_param_len = 23
+cmd_param_len = 25
 cmd_total_len = cmd_param_len + 4
 
 numHCIcmds = 1
-event_param_len = 19
-total_param_len = 22
-event_code = HCI_EVT_LE_META
-le_sub_event = HCI_SUB_EVT_LE_CONNECTION_COMPLETE
-
+event_param_len = 1
+total_param_len = 4
+event_code = HCI_EVT_CMD_STATUS
 ---------------------------------------------------------------------------------
 cmd = array.new(cmd_total_len)    -- 5
 cmd[1] = hci_type_cmd
@@ -34,7 +32,6 @@ print("<------------------------------------------------------------------------
 len = tl_usb_bulk_out(handle,cmd, cmd_total_len)
 
 
-
 --repeat
 --	resTbl,resLen = tl_usb_bulk_read()
 --	tl_sleep_ms(100)
@@ -48,6 +45,7 @@ do
 
    if(resLen > 0)
    then
+        print("resTbl:",resTbl[1],resTbl[2],resTbl[3],resTbl[4],resTbl[5],resTbl[6],resTbl[7],resTbl[8])
 		break
    end
 end
@@ -57,7 +55,7 @@ end
 local eventERR = 0
 
 ------------------------------------------------------------------------------    event Params
--- type_evt  evtCode(0e)  evtParamLen  numHciCmds   opCode_OCF   opCode_OGF         status
+-- type_evt  evtCode(0e)  evtParamLen   status      numHciCmds   opCode_OCF       opCode_OGF         
 --    1			1		  	   1		   1			1			 1			       1     
 
 --  total_param_len =  3 + event_param_len
@@ -75,12 +73,12 @@ end
 
 if(resTbl[1] == HCI_TYPE_EVENT and resTbl[2] == event_code)
 then
-	print(string.format("HCI_Command_Complete_Event") )
+	print(string.format("HCI_Command_Status_Event") )
 	print("-------------------------------------------------------------------------------------->")
-	print(string.format("Status: 0x%02x",resTbl[7])) 
+	print(string.format("Status: 0x%02x",resTbl[4])) 
 	
-	if( resTbl[3] == total_param_len and resTbl[4] == numHCIcmds and resTbl[5] == opcode_OCF and 
-		resTbl[6] == opcode_OGF and resTbl[7] == status)
+	if( resTbl[3] == total_param_len and resTbl[4] == status and resTbl[5] == numHCIcmds and 
+		resTbl[6] == opcode_OCF and resTbl[7] == opcode_OGF)
 	then
 		eventERR = 0  --event OK
 	else
@@ -128,16 +126,23 @@ then
 	end
 
 
-	--resTbl[4]: le_sub_event 
-	if(resTbl[4] == le_sub_event)  
+	--resTbl[4]: status 
+	if(resTbl[4] == status)  
 	then
-		print(string.format("0x%02x",resTbl[4]), "OK, le_sub_event")  
+		print(string.format("0x%02x",resTbl[4]), "OK, Command currently in pending.")  
 	else
-		print(string.format("0x%02x",resTbl[4]), "ERR, le_sub_event")   
+		print(string.format("0x%02x",resTbl[4]), "ERR, Command failed.")---Command Disallowed   
 	end
 
-	--resTbl[5]: status
-	if(resTbl[5] == status)  
+	--resTbl[5]: Num_HCI_Command_Packets
+	if(resTbl[5] == numHCIcmds)  
+	then
+		print(string.format("0x%02x",resTbl[5]), "OK, status")  
+	else
+		print(string.format("0x%02x",resTbl[5]), "ERR, status") 
+	end
+	
+	if(resTbl[6] == opcode_OCF and resTbl[7] == opcode_OGF)  
 	then
 		print(string.format("0x%02x",resTbl[5]), "OK, status")  
 	else
