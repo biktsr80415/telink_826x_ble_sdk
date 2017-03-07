f60ca2303c5d233b84ca70082107080ce661f849
wzt <zhitao.wu@telink-semi.com>
2017-03-07 09:43:55 +0800

    add hid service for profile_test proj

--- ble_lt_sdk/vendor/826x_profile_test/app_att.c
+++ ble_lt_sdk/vendor/826x_profile_test/app_att.c
@@ -264,6 +264,220 @@ u8 heart_rate_control_point_val;
 /////////////////////////////////////////////////////////////////////////////////////////////
 
 
+//////////////////////// HID /////////////////////////////////////////////////////
+
+const u16 my_hidServiceUUID       			= SERVICE_UUID_HUMAN_INTERFACE_DEVICE;
+const u16 my_SppDataServer2ClientUUID		= TELINK_SPP_DATA_SERVER2CLIENT;
+const u16 my_SppDataClient2ServiceUUID		= TELINK_SPP_DATA_CLIENT2SERVER;
+
+
+const u16 my_SppNameUUID		= GATT_UUID_CHAR_USER_DESC;
+const u8  my_SppName[] = {'m', 'y', 'S', 'P', 'P'};
+
+const u16 hidServiceUUID           = SERVICE_UUID_HUMAN_INTERFACE_DEVICE;
+const u16 hidProtocolModeUUID      = CHARACTERISTIC_UUID_HID_PROTOCOL_MODE;
+const u16 hidReportUUID            = CHARACTERISTIC_UUID_HID_REPORT;
+const u16 hidReportMapUUID         = CHARACTERISTIC_UUID_HID_REPORT_MAP;
+const u16 hidbootKeyInReportUUID   = CHARACTERISTIC_UUID_HID_BOOT_KEY_INPUT;
+const u16 hidbootKeyOutReportUUID  = CHARACTERISTIC_UUID_HID_BOOT_KEY_OUTPUT;
+const u16 hidbootMouseInReportUUID = CHARACTERISTIC_UUID_HID_BOOT_MOUSE_INPUT;
+const u16 hidinformationUUID       = CHARACTERISTIC_UUID_HID_INFORMATION;
+const u16 hidCtrlPointUUID         = CHARACTERISTIC_UUID_HID_CONTROL_POINT;
+const u16 hidIncludeUUID           = GATT_UUID_INCLUDE;
+// Protocol Mode characteristic variables
+static u8 protocolModeProp = CHAR_PROP_READ | CHAR_PROP_WRITE_WITHOUT_RSP;
+u8 protocolMode = DFLT_HID_PROTOCOL_MODE;
+
+
+// Key in Report characteristic variables
+u8 reportKeyInProp = CHAR_PROP_READ | CHAR_PROP_NOTIFY;
+u8 reportKeyIn[8];
+u8 reportKeyInCCC[2];
+// HID Report Reference characteristic descriptor, key input
+static u8 reportRefKeyIn[2] =
+             { HID_REPORT_ID_KEYBOARD_INPUT, HID_REPORT_TYPE_INPUT };
+
+// Key out Report characteristic variables
+u8 reportKeyOutProp = CHAR_PROP_READ | CHAR_PROP_WRITE | CHAR_PROP_WRITE_WITHOUT_RSP;
+u8 reportKeyOut[1];
+u8 reportKeyOutCCC[2];
+static u8 reportRefKeyOut[2] =
+             { HID_REPORT_ID_KEYBOARD_INPUT, HID_REPORT_TYPE_OUTPUT };
+
+// Consumer Control input Report
+static u8 reportConsumerControlInProp = CHAR_PROP_READ | CHAR_PROP_NOTIFY;
+static u8 reportConsumerControlIn[2];
+static u8 reportConsumerControlInCCC[2];
+static u8 reportRefConsumerControlIn[2] = {0x03,0x03};//{ HID_REPORT_ID_CONSUME_CONTROL_INPUT, HID_REPORT_TYPE_INPUT };//LCR-BV-04-V
+
+// HID Report characteristic, mouse input
+static u8 reportMouseInProp = CHAR_PROP_READ | CHAR_PROP_NOTIFY;
+static u8 reportMouseIn[4];
+static u8 reportMouseInCCC[2];
+static u8 reportRefMouseIn[2] = { HID_REPORT_ID_MOUSE_INPUT, HID_REPORT_TYPE_INPUT };
+
+
+// Boot Keyboard Input Report
+static u8 bootKeyInReportProp = CHAR_PROP_READ | CHAR_PROP_NOTIFY;
+static u8 bootKeyInReport[2];
+static u8 bootKeyInReportCCC[2];
+
+// Boot Keyboard Output Report
+static u8 bootKeyOutReportProp = CHAR_PROP_READ | CHAR_PROP_WRITE | CHAR_PROP_WRITE_WITHOUT_RSP;
+static u8 bootKeyOutReport[2];
+static u8 bootKeyOutReportCCC[2];
+
+// Boot Mouse Input Report
+static u8 bootMouseInReportProp = CHAR_PROP_READ | CHAR_PROP_WRITE | CHAR_PROP_NOTIFY;
+static u8 bootMouseInReport[2];
+static u8 bootMouseInReportCCC[2] = {0x00,0x00};
+
+// HID Information characteristic
+static u8 hidInfoProps = CHAR_PROP_READ;
+const u8 hidInformation[] =
+{
+  U16_LO(0x0111), U16_HI(0x0111),             // bcdHID (USB HID version)
+  0x00,                                       // bCountryCode
+  0x01                                        // Flags
+};
+
+// HID Control Point characteristic
+static u8 controlPointProp = CHAR_PROP_WRITE_WITHOUT_RSP;
+static u8 controlPoint;
+
+// HID Report Map characteristic
+static u8 reportMapProp = CHAR_PROP_READ;
+// Keyboard report descriptor (using format for Boot interface descriptor)
+
+static const u8 reportMap[] =
+{
+    0x05, 0x01,  // Usage Page (Generic Desktop)
+    0x09, 0x02,  // Usage (Mouse)
+    0xA1, 0x01,  // Collection (Application)
+    0x85, 0x01,  // Report Id (1)
+    0x09, 0x01,  //   Usage (Pointer)
+    0xA1, 0x00,  //   Collection (Physical)
+    0x05, 0x09,  //     Usage Page (Buttons)
+    0x19, 0x01,  //     Usage Minimum (01) - Button 1
+    0x29, 0x03,  //     Usage Maximum (03) - Button 3
+    0x15, 0x00,  //     Logical Minimum (0)
+    0x25, 0x01,  //     Logical Maximum (1)
+    0x75, 0x01,  //     Report Size (1)
+    0x95, 0x03,  //     Report Count (3)
+    0x81, 0x02,  //     Input (Data, Variable, Absolute) - Button states
+    0x75, 0x05,  //     Report Size (5)
+    0x95, 0x01,  //     Report Count (1)
+    0x81, 0x01,  //     Input (Constant) - Padding or Reserved bits
+    0x05, 0x01,  //     Usage Page (Generic Desktop)
+    0x09, 0x30,  //     Usage (X)
+    0x09, 0x31,  //     Usage (Y)
+    0x09, 0x38,  //     Usage (Wheel)
+    0x15, 0x81,  //     Logical Minimum (-127)
+    0x25, 0x7F,  //     Logical Maximum (127)
+    0x75, 0x08,  //     Report Size (8)
+    0x95, 0x03,  //     Report Count (3)
+    0x81, 0x06,  //     Input (Data, Variable, Relative) - X & Y coordinate
+    0xC0,        //   End Collection
+    0xC0,        // End Collection
+
+    #if 1
+    // Report ID 3 consumer report,  media key
+
+    0x05, 0x0C,   // Usage Page (Consumer)
+    0x09, 0x01,   // Usage (Consumer Control)
+    0xA1, 0x01,   // Collection (Application)
+    0x85, 0x02,   //     Report Id (3)
+
+    0x09, 0xE9,   //   Usage (Volume Up)            //0x0001
+    0x09, 0xEA,   //   Usage (Volume Down)			//0x0002
+    0x15, 0x00,   //   Logical Min (0)
+    0x75, 0x01,   //   Report Size (1)
+    0x95, 0x02,   //   Report Count (2)
+    0x81, 0x02,   //   Input (Data, Var, Abs)
+    0x09, 0xE2,   //   Usage (Mute)					//0x0004, 1<<2
+    0x09, 0x30,   //   Usage (Power)				//0x0008, 2<<2
+    0x09, 0x41,   //   Usage (Menu Pick)			//0x000c, 3<<2
+    0x09, 0x42,   //   Usage (Menu Up)				//0x0010, 4<<2
+    0x09, 0x43,   //   Usage (Menu Down)			//0x0014, 5<<2
+    0x09, 0x44,   //   Usage (Menu Left)			//0x0018, 6<<2
+    0x09, 0x45,   //   Usage (Menu Right)			//0x001c, 7<<2
+    0x0a, 0x23, 0x02,   //   Usage (HOME)			//0x0020, 8<<2
+    0x09, 0xB4,   //   Usage (Rewind)				//0x0024, 9<<2
+    0x09, 0xB5,   //   Usage (Scan Next)			//0x0028, a<<2
+    0x09, 0xB6,   //   Usage (Scan Prev)			//0x002c, b<<2
+    0x09, 0xB7,   //   Usage (Stop)					//0x0030, c<<2
+    0x15, 0x01,   //   Logical Min (1)
+    0x25, 0x0C,   //   Logical Max (12)
+    0x75, 0x04,   //   Report Size (4)
+    0x95, 0x01,   //   Report Count (1)
+    0x81, 0x00,   //   Input (Data, Ary, Abs)
+    0x09, 0x80,   //   Usage (Selection)
+    0xA1, 0x02,   //   Collection (Logical)
+    0x05, 0x09,   //     Usage Pg (Button)
+    0x19, 0x01,   //     Usage Min (Button 1)
+    0x29, 0x03,   //     Usage Max (Button 3)
+    0x15, 0x01,   //     Logical Min (1)
+    0x25, 0x03,   //     Logical Max (3)
+    0x75, 0x02,   //     Report Size (2)
+    0x81, 0x00,   //     Input (Data, Ary, Abs)
+    0xC0,         //   End Collection
+    0x81, 0x03,   //   Input (Const, Var, Abs)
+    0xC0,         // End Collection
+#endif
+
+
+    0x05, 0x01,     // Usage Pg (Generic Desktop)
+    0x09, 0x06,     // Usage (Keyboard)
+    0xA1, 0x01,     // Collection: (Application)
+    0x85, 0x03,     // Report Id (3 for keyboard)
+                  //
+    0x05, 0x07,     // Usage Pg (Key Codes)
+    0x19, 0xE0,     // Usage Min (224)  VK_CTRL:0xe0
+    0x29, 0xE7,     // Usage Max (231)  VK_RWIN:0xe7
+    0x15, 0x00,     // Log Min (0)
+    0x25, 0x01,     // Log Max (1)
+                  //
+                  // Modifier byte
+    0x75, 0x01,     // Report Size (1)   1 bit * 8
+    0x95, 0x08,     // Report Count (8)
+    0x81, 0x02,     // Input: (Data, Variable, Absolute)
+                  //
+                  // Reserved byte
+    0x95, 0x01,     // Report Count (1)
+    0x75, 0x08,     // Report Size (8)
+    0x81, 0x01,     // Input: (Constant)
+
+    //keyboard output
+    //5 bit led ctrl: NumLock CapsLock ScrollLock Compose kana
+    0x95, 0x05,    //Report Count (5)
+    0x75, 0x01,    //Report Size (1)
+    0x05, 0x08,    //Usage Pg (LEDs )
+    0x19, 0x01,    //Usage Min
+    0x29, 0x05,    //Usage Max
+    0x91, 0x02,    //Output (Data, Variable, Absolute)
+    //3 bit reserved
+    0x95, 0x01,    //Report Count (1)
+    0x75, 0x03,    //Report Size (3)
+    0x91, 0x01,    //Output (Constant)
+
+	// Key arrays (6 bytes)
+    0x95, 0x06,     // Report Count (6)
+    0x75, 0x08,     // Report Size (8)
+    0x15, 0x00,     // Log Min (0)
+    0x25, 0xF1,     // Log Max (241)
+    0x05, 0x07,     // Usage Pg (Key Codes)
+    0x19, 0x00,     // Usage Min (0)
+    0x29, 0xf1,     // Usage Max (241)
+    0x81, 0x00,     // Input: (Data, Array)
+
+    0xC0,            // End Collection
+};
+
+// HID External Report Reference Descriptor for report map
+static u16 extServiceUUID;
+// Include attribute (Battery service)
+static u16 include[3] = {0x0026, 0x0028, SERVICE_UUID_BATTERY};
+//////////////////////////////////////////////////////////////////////////////////////////////////
 
 // TM : to modify
 const attribute_t my_Attributes[] = {
@@ -539,6 +753,85 @@ const attribute_t my_Attributes[] = {
     {3,ATT_PERMISSIONS_READ,2,2,(u8*)(&my_primaryServiceUUID), 	(u8*)(&txPower_serviceUUID), 0},
     {0,ATT_PERMISSIONS_READ,2,1,(u8*)(&my_characterUUID), 		(u8*)(&txPowerLevel_prop), 0},
     {0,ATT_PERMISSIONS_READ,2,1,(u8*)(&txPowerLevel_charUUID), (u8*)(&txPowerLevel_value), 0},
+#else
+    {47,0,0,0,0,0},	// total num of attribute //hid
+	// 0001 - 0007  gap
+    {7,ATT_PERMISSIONS_READ,2,2,(u8*)(&my_primaryServiceUUID), 	(u8*)(&my_gapServiceUUID), 0},
+	{0,ATT_PERMISSIONS_READ,2,1,(u8*)(&my_characterUUID), 		(u8*)(&my_devNameCharacter), 0},
+	{0,ATT_PERMISSIONS_READ,2,MAX_DEV_NAME_LEN, (u8*)(&my_devNameUUID), (u8*)(&ble_devName), 0},
+	{0,ATT_PERMISSIONS_READ,2,1,(u8*)(&my_characterUUID), 		(u8*)(&my_appearanceCharacter), 0},
+	{0,ATT_PERMISSIONS_READ,2,sizeof (my_appearance), (u8*)(&my_appearanceUIID), 	(u8*)(&my_appearance), 0},
+	{0,ATT_PERMISSIONS_READ,2,1,(u8*)(&my_characterUUID), 		(u8*)(&my_periConnParamChar), 0},
+	{0,ATT_PERMISSIONS_READ,2,sizeof (my_periConnParameters),(u8*)(&my_periConnParamUUID), 	(u8*)(&my_periConnParameters), 0},
+
+	// 0008 - 000a  device Information Service
+	{3,ATT_PERMISSIONS_READ,2,2,(u8*)(&my_primaryServiceUUID), 	(u8*)(&my_devServiceUUID), 0},
+	{0,ATT_PERMISSIONS_READ,2,1,(u8*)(&my_characterUUID), 		(u8*)(&pnpID_prop), 0},
+	{0,ATT_PERMISSIONS_READ,2,sizeof (pnpID_value),(u8*)(&pnpID_charUUID), (u8*)(pnpID_value), 0},
+	/////////////////////////////////// 4. HID Service /////////////////////////////////////////////////////////
+	// 000b
+	{34,ATT_PERMISSIONS_READ,2,2,(u8*)(&my_primaryServiceUUID), 	(u8*)(&my_hidServiceUUID), 0},
+	// 000c - 000e  include battery service
+	{0,ATT_PERMISSIONS_READ,2,sizeof(include),(u8*)(&hidIncludeUUID), 	(u8*)(include), 0},
+
+	// 000d - 000e  protocol mode
+	{0,ATT_PERMISSIONS_READ,2,1,(u8*)(&my_characterUUID), 		(u8*)(&protocolModeProp), 0},				//prop
+	{0,ATT_PERMISSIONS_RDWR,2, sizeof(protocolMode),(u8*)(&hidProtocolModeUUID), 	(u8*)(&protocolMode), 0},	//value
+
+	// 000f - 0011  boot keyboard input report (char-val-client)
+	{0,ATT_PERMISSIONS_READ,2,1,(u8*)(&my_characterUUID), 		(u8*)(&bootKeyInReportProp), 0},				//prop
+	{0,ATT_PERMISSIONS_RDWR,2,sizeof(bootKeyInReport),(u8*)(&hidbootKeyInReportUUID), 	(u8*)(&bootKeyInReport), 0},	//value
+	{0,ATT_PERMISSIONS_RDWR,2,sizeof(bootKeyInReportCCC),(u8*)(&clientCharacterCfgUUID), 	(u8*)(bootKeyInReportCCC), 0},	//value
+
+	// 0012 - 0013   boot keyboard output report (char-val)
+	{0,ATT_PERMISSIONS_READ,2,1,(u8*)(&my_characterUUID), 		(u8*)(&bootKeyOutReportProp), 0},				//prop
+	{0,ATT_PERMISSIONS_RDWR,2, sizeof(bootKeyOutReport), (u8*)(&hidbootKeyOutReportUUID), 	(u8*)(&bootKeyOutReport), 0},	//value
+
+	//0014 - 0016    boot mouse input report
+	{0,ATT_PERMISSIONS_READ,2,  1,(u8*)(&my_characterUUID), 		(u8*)(&bootMouseInReportProp), 0},
+	{0,ATT_PERMISSIONS_RDWR,2,  sizeof(bootMouseInReport),(u8*)(&hidbootMouseInReportUUID), 	(u8*)(&bootMouseInReport), 0},
+	{0,ATT_PERMISSIONS_RDWR,2,  sizeof(bootMouseInReportCCC),(u8*)(&clientCharacterCfgUUID), 	(u8*)(bootMouseInReportCCC), 0},
+
+	//0017 - 001A  Characteristic declaration: Report (Mouse In)
+	{0,ATT_PERMISSIONS_READ,2,  1,(u8*)(&my_characterUUID), 		(u8*)(&reportMouseInProp), 0},
+	{0,ATT_PERMISSIONS_RDWR,2,  sizeof(reportMouseIn),(u8*)(&hidReportUUID), 	(u8*)(&reportMouseIn), 0},
+	{0,ATT_PERMISSIONS_RDWR,2,  sizeof(reportMouseInCCC),(u8*)(&clientCharacterCfgUUID), 	(u8*)(reportMouseInCCC), 0},
+	{0,ATT_PERMISSIONS_RDWR,2, sizeof(reportRefMouseIn),(u8*)(&reportRefUUID), 	(u8*)(reportRefMouseIn), 0},
+
+	// 001B - 001E. consume report in: 4 (char-val-client-ref)
+	{0,ATT_PERMISSIONS_READ,2,1,(u8*)(&my_characterUUID), 		(u8*)(&reportConsumerControlInProp), 0},				//prop
+	{0,ATT_PERMISSIONS_RDWR,2, sizeof(reportConsumerControlIn),(u8*)(&hidReportUUID), 	(u8*)(reportConsumerControlIn), 0},	//value
+	{0,ATT_PERMISSIONS_READ|ATT_PERMISSIONS_AUTHEN_WRITE,2,sizeof(reportConsumerControlInCCC),(u8*)(&clientCharacterCfgUUID), 	(u8*)(reportConsumerControlInCCC), 0},	//value
+	{0,ATT_PERMISSIONS_RDWR,2,sizeof(reportRefConsumerControlIn),(u8*)(&reportRefUUID), 	(u8*)(reportRefConsumerControlIn), 0},	//value
+
+	// 001F - 0022 . report in : 4 (char-val-client-ref)
+	{0,ATT_PERMISSIONS_READ,2,1,(u8*)(&my_characterUUID), 		(u8*)(&reportKeyInProp), 0},				//prop
+	{0,ATT_PERMISSIONS_RDWR,2, sizeof(reportKeyIn),(u8*)(&hidReportUUID), 	(u8*)(reportKeyIn), 0},	//value
+	{0,ATT_PERMISSIONS_READ|ATT_PERMISSIONS_AUTHEN_WRITE,2,sizeof(reportKeyInCCC),(u8*)(&clientCharacterCfgUUID), 	(u8*)(reportKeyInCCC), 0},	//value
+	{0,ATT_PERMISSIONS_RDWR,2,sizeof(reportRefKeyIn),(u8*)(&reportRefUUID), 	(u8*)(reportRefKeyIn), 0},	//value
+
+	// 0023 - 0025 . report out: 3 (char-val-ref)
+	{0,ATT_PERMISSIONS_READ,2,1,(u8*)(&my_characterUUID), 		(u8*)(&reportKeyOutProp), 0},				//prop
+	{0,ATT_PERMISSIONS_RDWR,2,sizeof(reportKeyOut),(u8*)(&hidReportUUID), 	(u8*)(reportKeyOut), 0},	//value
+	{0,ATT_PERMISSIONS_RDWR,2,sizeof(reportRefKeyOut),(u8*)(&reportRefUUID), 	(u8*)(reportRefKeyOut), 0},	//value
+
+	// 0026 - 0028 . report map: 3
+	{0,ATT_PERMISSIONS_READ,2,1,(u8*)(&my_characterUUID), 		(u8*)(&reportMapProp), 0},				//prop
+	{0,ATT_PERMISSIONS_READ,2,sizeof(reportMap),(u8*)(&hidReportMapUUID), 	(u8*)(reportMap), 0},	//value
+	{0,ATT_PERMISSIONS_READ|ATT_PERMISSIONS_WRITE,2,sizeof(extServiceUUID),(u8*)(&extReportRefUUID), 	(u8*)(&extServiceUUID), 0},	//value
+
+	// 0029 - 002A . hid information: 2
+	{0,ATT_PERMISSIONS_READ,2,1,(u8*)(&my_characterUUID), 		(u8*)(&hidInfoProps), 0},				//prop
+	{0,ATT_PERMISSIONS_READ,2, sizeof(hidInformation),(u8*)(&hidinformationUUID), 	(u8*)(hidInformation), 0},	//value
+
+	// 002B - 002C . control point: 2
+	{0,ATT_PERMISSIONS_READ,2,1,(u8*)(&my_characterUUID), 		(u8*)(&controlPointProp), 0},				//prop
+	{0,ATT_PERMISSIONS_WRITE,2, sizeof(controlPoint),(u8*)(&hidCtrlPointUUID), 	(u8*)(&controlPoint), 0},	//value
+	////////////////////////////////////// 31. Battery Service /////////////////////////////////////////////////////
+	// 002C - 002E
+	{3,ATT_PERMISSIONS_READ,2,2,(u8*)(&my_primaryServiceUUID), 	(u8*)(&my_batServiceUUID), 0},
+	{0,ATT_PERMISSIONS_READ,2,1,(u8*)(&my_characterUUID), 		(u8*)(&my_batProp), 0},				//prop
+	{0,ATT_PERMISSIONS_READ,2,sizeof(my_batVal),(u8*)(&my_batCharUUID), 	(u8*)(my_batVal), 0},	//value
 #endif
 
 };
