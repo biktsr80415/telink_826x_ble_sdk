b9b197d71e53b911dfbbcaafd981cded9a33485b
sihui.wang <sihui.wang@telink-semi.com>
2017-03-24 14:35:17 +0800

    fix compile err

--- ble_lt_sdk/proj/drivers/uart.c
+++ ble_lt_sdk/proj/drivers/uart.c
@@ -386,7 +386,7 @@ void uart_io_init(unsigned char uart_io_sel){
 #endif
 }
 
-#if(MCU_CORE_TYPE == MCU_CORE_8267)
+#if 0// (MCU_CORE_TYPE == MCU_CORE_8267)
 /**
  * @brief UART hardware flow control configuration. Configure RTS pin.
  * @param[in]   enable: enable or disable RTS function.
