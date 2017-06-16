8cd41cea66a090341d6286df8987ba7ebaf602b5
sihui.wang <sihui.wang@telink-semi.com>
2017-06-16 11:11:41 +0800

    remove files

--- tools/btstack/vcsim/StdAfx.cpp
+++
@@ -1,8 +0,0 @@
-// stdafx.cpp : source file that includes just the standard includes
-//	tl_ble_module.pch will be the pre-compiled header
-//	stdafx.obj will contain the pre-compiled type information
-
-#include "stdafx.h"
-
-
-
