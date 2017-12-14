b5d3769eeccc125601963a0920d7d8b8a4432dd6
yafei.tu <yafei.tu@telink-semi.com>
2017-12-14 14:50:16 +0800

    update myprintf

--- ble_lt_sdk/vendor/common/myprintf.c
+++ ble_lt_sdk/vendor/common/myprintf.c
@@ -23,119 +23,143 @@
 #define		BIT_INTERVAL	(CLOCK_SYS_CLOCK_HZ/PRINT_BAUD_RATE)
 #endif
 
-_attribute_ram_code_ static void uart_put_char(u8 byte){
-	u8 j = 0;
-	u32 t1 = 0,t2 = 0;
+_attribute_ram_code_ static void uart_put_char(char **str, u8 byte){
 	
-	REG_ADDR8(0x582+((DEBUG_INFO_TX_PIN>>8)<<3)) &= ~(DEBUG_INFO_TX_PIN & 0xff) ;//Enable output
+	if (str) {
+		**str = byte;
+		++(*str);
+	} else{
+		u8 j = 0;
+		u32 t1 = 0,t2 = 0;
+
+		REG_ADDR8(0x582+((DEBUG_INFO_TX_PIN>>8)<<3)) &= ~(DEBUG_INFO_TX_PIN & 0xff) ;//Enable output
 
 
-	u32 pcTxReg = (0x583+((DEBUG_INFO_TX_PIN>>8)<<3));//register GPIO output
-	u8 tmp_bit0 = read_reg8(pcTxReg) & (~(DEBUG_INFO_TX_PIN & 0xff));
-	u8 tmp_bit1 = read_reg8(pcTxReg) | (DEBUG_INFO_TX_PIN & 0xff);
+		u32 pcTxReg = (0x583+((DEBUG_INFO_TX_PIN>>8)<<3));//register GPIO output
+		u8 tmp_bit0 = read_reg8(pcTxReg) & (~(DEBUG_INFO_TX_PIN & 0xff));
+		u8 tmp_bit1 = read_reg8(pcTxReg) | (DEBUG_INFO_TX_PIN & 0xff);
 
 
-	u8 bit[10] = {0};
-	bit[0] = tmp_bit0;
-	bit[1] = (byte & 0x01)? tmp_bit1 : tmp_bit0;
-	bit[2] = ((byte>>1) & 0x01)? tmp_bit1 : tmp_bit0;
-	bit[3] = ((byte>>2) & 0x01)? tmp_bit1 : tmp_bit0;
-	bit[4] = ((byte>>3) & 0x01)? tmp_bit1 : tmp_bit0;
-	bit[5] = ((byte>>4) & 0x01)? tmp_bit1 : tmp_bit0;
-	bit[6] = ((byte>>5) & 0x01)? tmp_bit1 : tmp_bit0;
-	bit[7] = ((byte>>6) & 0x01)? tmp_bit1 : tmp_bit0;
-	bit[8] = ((byte>>7) & 0x01)? tmp_bit1 : tmp_bit0;
-	bit[9] = tmp_bit1;
+		u8 bit[10] = {0};
+		bit[0] = tmp_bit0;
+		bit[1] = (byte & 0x01)? tmp_bit1 : tmp_bit0;
+		bit[2] = ((byte>>1) & 0x01)? tmp_bit1 : tmp_bit0;
+		bit[3] = ((byte>>2) & 0x01)? tmp_bit1 : tmp_bit0;
+		bit[4] = ((byte>>3) & 0x01)? tmp_bit1 : tmp_bit0;
+		bit[5] = ((byte>>4) & 0x01)? tmp_bit1 : tmp_bit0;
+		bit[6] = ((byte>>5) & 0x01)? tmp_bit1 : tmp_bit0;
+		bit[7] = ((byte>>6) & 0x01)? tmp_bit1 : tmp_bit0;
+		bit[8] = ((byte>>7) & 0x01)? tmp_bit1 : tmp_bit0;
+		bit[9] = tmp_bit1;
 
-	//u8 r = irq_disable();
-	t1 = read_reg32(0x740);
-	for(j = 0;j<10;j++)
-	{
-		t2 = t1;
-		while(t1 - t2 < BIT_INTERVAL){
-			t1  = read_reg32(0x740);
+		//u8 r = irq_disable();
+		t1 = read_reg32(0x740);
+		for(j = 0;j<10;j++)
+		{
+			t2 = t1;
+			while(t1 - t2 < BIT_INTERVAL){
+				t1  = read_reg32(0x740);
+			}
+			write_reg8(pcTxReg,bit[j]);        //send bit0
 		}
-		write_reg8(pcTxReg,bit[j]);        //send bit0
+		//irq_restore(r);
 	}
-	//irq_restore(r);
 }
 
-static int puts(char *s){
+static int puts(char **out, char *s){
 	while((*s != '\0')){
-		uart_put_char(*s++);
+		uart_put_char(out, *s++);
 	}
 }
 
-static void puti(unsigned int num, int base){
+static void puti(char **out, unsigned int num, int base){
 	char re[]="0123456789ABCDEF";
 
-	char buf[50];
+	char buf[50], cnt = 0;
 
 	char *addr = &buf[49];
 
 	*addr = '\0';
 
 	do{
+		cnt++;
 		*--addr = re[num%base];
+		//when num < 0x10, high bit zero fill: like this: 0x00~0x0F
+		if(num < 0x10 && cnt == 1 && HEX_OUTPUT == base){
+			*--addr = '0';
+			break;
+		}
 		num/=base;
 	}while(num!=0);
 
-	puts(addr);
+	puts(out, addr);
 }
 
-
-int mini_printf(const char *format, ...){
-
+static int print(char **out, const char *format, va_list arg_ptr) {
 	char span;
 	unsigned long j;
 	char *s;
 	//char *msg;
-	va_list arg_ptr;
-	va_start(arg_ptr, format);
 
 	while((span = *(format++))){
 		if(span != '%'){
-			uart_put_char(span);
+			uart_put_char(out, span);
 		}else{
 			span = *(format++);
 			if(span == 'c'){
 				j = va_arg(arg_ptr,int);//get value of char
-				uart_put_char(j);
+				uart_put_char(out, j);
 			}else if(span == 'd'){
 				j = va_arg(arg_ptr,int);//get value of char
 				if(j<0){
-					uart_put_char('-');
+					uart_put_char(out, '-');
 					j = -j;
 				}
-				puti(j,DECIMAL_OUTPUT);
+				puti(out, j,DECIMAL_OUTPUT);
 			}else if(span == 's'){
 				s = va_arg(arg_ptr,char *);//get string value
-				puts(s);
+				puts(out, s);
 			}else if(span == 'o'){
 				j = va_arg(arg_ptr,unsigned int);//get octal value
-				puti(j,OCTAL_OUTPUT);
+				puti(out, j,OCTAL_OUTPUT);
 			}else if(span == 'x'){
 					j = va_arg(arg_ptr,unsigned int);//get hex value
-					puti(j,HEX_OUTPUT);
+					puti(out, j,HEX_OUTPUT);
 			}else if(span == 0){
 				break;
 			}else{
-				uart_put_char(span);
+				uart_put_char(out, span);
 			}
 		}
 
 	}
+	if (out){
+		**out = '\0';
+	}
+
 	va_end(arg_ptr);
 }
 
-u8 HexTable[]={'0','1','2','3','4','5','6','7','8','9','A','B','C','D','E','F'};
-void PrintHex(u8 x)
-{
-	uart_put_char('0');
-	uart_put_char('x');
-	uart_put_char(HexTable[x>>4]);
-	uart_put_char(HexTable[x&0xf]);
-	uart_put_char(' ');
+int mini_printf(const char *format, ...){
+	va_list arg_ptr;
+	va_start(arg_ptr, format);
+	return print(0, format, arg_ptr);
+}
+
+int mini_sprintf(char *out, const char *format, ...) {
+	va_list args;
+	va_start( args, format );
+	return print(&out, format, args);
+}
+
+void PrintHex(unsigned char x){
+    //u8 HexTable[]={'0','1','2','3','4','5','6','7','8','9','A','B','C','D','E','F'};
+	//uart_put_char(0, '0');
+	//uart_put_char(0, 'x');
+	//uart_put_char(0, HexTable[x>>4]);
+	//uart_put_char(0, HexTable[x&0xf]);
+	//uart_put_char(0, ' ');
+	mini_printf("%x ", x);
 }
 
 #endif
--- ble_lt_sdk/vendor/common/myprintf.h
+++ ble_lt_sdk/vendor/common/myprintf.h
@@ -3,27 +3,27 @@
 
 #if (PRINT_DEBUG_INFO)
 
-void my_array_printf(char*data, int len);
 int mini_printf(const char *format, ...);
-void PrintHex(u8 x);
+int mini_sprintf(char *out, const char *format, ...);
+void PrintHex(unsigned char x);
 
 #define printf			mini_printf
 #define	printfArray		arrayPrint
-//#define	logPrint		mini_printf
-//#define	logPrintArray	arrayPrint
+#define sprintf         mini_sprintf
+
 #define	arrayPrint(arrayAddr,len)					\
 {													\
-	mini_printf("\n*********************************\n");		\
 	unsigned char	i = 0;							\
 	do{												\
-		mini_printf(" %x",((unsigned char *)arrayAddr)[i++]);						\
-	}while(i<len);										\
-	mini_printf("\n*********************************\n");		\
+		mini_printf("%x%s", ((unsigned char *)arrayAddr)[i++], i<len? "-":" "); \
+	}while(i<len); \
+	mini_printf("\n");	\
 }
 #else
 #define printf
 #define	printfArray
 #define	PrintHex
+#define sprintf
 #endif
 
 //#define debugBuffer (*(volatile unsigned char (*)[40])(0x8095d8))
