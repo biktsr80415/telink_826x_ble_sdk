df43b6011abb1da9c06c64aa9bebefe11d5ac655
sihui.wang <sihui.wang@telink-semi.com>
2018-05-22 11:10:36 +0800

    [Kite ble SDK] remove unused files

--- ble_sdk_multimode/vendor/common/user_config.h
+++ ble_sdk_multimode/vendor/common/user_config.h
@@ -1,10 +1,8 @@
 
 #pragma once
 
-#if (__PROJECT_8255_BLE_REMOTE__)
-	#include "../8255_ble_remote/app_config.h"
-#elif (__PROJECT_8255_DRIVER_TEST__)
-	#include "../8255_driver_test/app_config.h"
+#if (__PROJECT_8258_DRIVER_TEST__)
+	#include "../8258_driver_test/app_config.h"
 #elif (__PROJECT_8258_BLE_REMOTE__)
 	#include "../8258_ble_remote/app_config.h"
 #elif (__PROJECT_8258_BLE_SAMPLE__)
