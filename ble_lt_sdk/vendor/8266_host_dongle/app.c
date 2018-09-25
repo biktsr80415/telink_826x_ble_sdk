d01675a860229283d6bc5e6fb362a5ff4c573f75
yafei.tu <yafei.tu@telink-semi.com>
2018-09-25 09:50:59 +0800

    update 8266 host dongle

--- ble_lt_sdk/vendor/8266_host_dongle/app.c
+++ ble_lt_sdk/vendor/8266_host_dongle/app.c
@@ -18,31 +18,26 @@
 #include "../../proj/drivers/uart.h"
 
 //////////////////////// UART variables ///////////////////////////
-unsigned char rx_uart_r_index;
-unsigned char rx_uart_w_index;
-uart_data_t T_txdata_buf;
-uart_data_t T_rxdata_user;
-uart_data_t T_rxdata_buf[2];   // data max 252, user must copy rxdata to other Ram,but not use directly
-///////////////////////////////////////////////////////////////////
+MYFIFO_INIT(uart_txfifo, 	128, 4);
+MYFIFO_INIT(uart_rxfifo, 	128, 4);
 
-MYFIFO_INIT(hci_tx_fifo, 72, 4);
+#define MY_UART_DATA_LEN    128      // data max 252
+typedef struct{
+    unsigned int len;        // data max 252
+    unsigned char data[MY_UART_DATA_LEN];
+}myuart_data_t;
 
-int		dongle_pairing_enable;
+
+//////////////////////// USB variables ///////////////////////////
+MYFIFO_INIT(usb_txfifo, 	128, 4);
+
+
+
 u8		spp_test_en = 0;
 u32		spp_num = 0;
 u16		spp_conn = 0;
 u16		spp_handle = 0;
 u32		spp_err = 0;
-
-///////////////////////////////////////////
-// TX FIFO
-///////////////////////////////////////////
-#define			HCI_BUFFER_TX_SIZE		72
-#define			HCI_BUFFER_NUM			4
-
-MYFIFO_INIT(uart_fifo, HCI_BUFFER_TX_SIZE, HCI_BUFFER_NUM);
-MYFIFO_INIT(usb_fifo, HCI_BUFFER_TX_SIZE, HCI_BUFFER_NUM);
-
 //------------------- spp test ----------------------------------------------
 void app_send_spp_status ()
 {
@@ -54,14 +49,13 @@ void app_send_spp_status ()
 	dat[3] = 0x07;
 	memcpy (dat + 4, &spp_num, 4);
 	memcpy (dat + 8, &spp_err, 4);
-	my_fifo_push (&usb_fifo, dat, 12);
+	my_fifo_push (&usb_txfifo, dat, 12);
 }
-
 //////////////////////////////////// SPP test code ///////////////////////////////
 int	 spp_write = 0;
 void spp_test_write ()
 {
-	if (!spp_write && spp_num && spp_handle && my_fifo_wptr (&uart_fifo))
+	if (!spp_write && spp_num && spp_handle && my_fifo_wptr (&uart_txfifo))
 	{
 		u8		dat[32];
 		u8 *p = dat;
@@ -77,7 +71,7 @@ void spp_test_write ()
 			p[i] = p[0] + i;
 		}
 
-		if (my_fifo_push (&uart_fifo, dat, 20 + (p - dat)) == 0)
+		if (my_fifo_push (&uart_txfifo, dat, 20 + (p - dat)) == 0)
 		{
 			spp_num--;
 			spp_write = 1;
@@ -88,7 +82,6 @@ void spp_test_write ()
 		}
 	}
 }
-
 void	spp_test_read (u8 *p, int n)
 {
 	static u32 spp_err_st;
@@ -124,12 +117,77 @@ void	spp_test_read (u8 *p, int n)
 	}
 }
 
-//--------------------	usb -------------------------------------------------
-int app_packet_from_usb (void)
+//--------------------	USB -------------------------------------------------
+/////////////////////////////////////////////////////
+// write flash memory through swire
+/////////////////////////////////////////////////////
+#define			USB_ENDPOINT_BULK_IN			8
+#define			USB_ENDPOINT_BULK_OUT			5
+#define			USB_ENDPOINT_BULK_OUT_FLAG		(1 << (USB_ENDPOINT_BULK_OUT & 7))
+
+
+static u32 myusb_endpoint_busy(u32 ep) {
+	write_reg8 (0x80013d, 0);
+	return read_reg8 (0x800120 + (ep&7)) & 1;
+}
+
+// 0: last packet; -1: busy
+int myusb_bulk_in_packet (u8 *p, int n)
 {
-	u8 buff[72];
-	extern int usb_bulk_out_get_data (u8 *p);
-	int n = usb_bulk_out_get_data (buff);
+	static u32 offset = 0;
+
+	if( myusb_endpoint_busy(USB_ENDPOINT_BULK_IN))
+		return -1;
+
+
+	if (n <= 64)
+	{
+		offset = 0;
+	}
+	else
+	{
+		n -= offset;
+	}
+
+	//reset pointer to 0
+	write_reg8 (0x800110 + (USB_ENDPOINT_BULK_IN & 7), 0);
+	for (int i=0; i<(n >= 64 ? 64 : n); i++)
+	{
+		write_reg8 (0x800118 + (USB_ENDPOINT_BULK_IN & 7), p[offset + i]);
+	}
+	write_reg8 (0x800120 + (USB_ENDPOINT_BULK_IN & 7), 1);		//ACK
+
+	offset = n <= 64 ? 0 : offset + 64;
+
+	return offset;
+}
+
+int myusb_bulk_out_get_data (u8 *p)
+{
+	if (read_reg8 (0x800139) & USB_ENDPOINT_BULK_OUT_FLAG)
+	{
+		//clear interrupt flag
+		write_reg8 (0x800139, USB_ENDPOINT_BULK_OUT_FLAG);
+
+		// read data
+		int n = read_reg8 (0x800110 + (USB_ENDPOINT_BULK_OUT & 7));
+		write_reg8 (0x800110 + (USB_ENDPOINT_BULK_OUT & 7), 0);
+		for (int i=0; i<n; i++)
+		{
+			p[i] = read_reg8 (0x800118 + (USB_ENDPOINT_BULK_OUT & 7));
+		}
+		write_reg8 (0x800120 + (USB_ENDPOINT_BULK_OUT & 7), 1);		//ACK
+
+		return n;
+	}
+	return 0;
+}
+
+
+int app_data_from_usb (void)
+{
+	u8 buff[128];
+	int n = myusb_bulk_out_get_data (buff);
 	if (n)
 	{
 		if (buff[0] == 0xe0 && buff[1] == 0xff)		//set mac address filter
@@ -152,38 +210,39 @@ int app_packet_from_usb (void)
 		}
 		else	// send to UART
 		{
-			my_fifo_push (&uart_fifo, buff, n);
+			my_fifo_push (&uart_txfifo, buff, n);
 		}
 	}
 	return 0;
 }
 
-int app_packet_to_usb ()
+int app_data_to_usb ()
 {
-	u8 *p = my_fifo_get (&usb_fifo);
-	extern int usb_bulk_in_packet (u8 *p, int n);
-	if (p && usb_bulk_in_packet (p + 2, p[0] + p[1] * 256) == 0)
-	{
-		my_fifo_pop (&usb_fifo);
+	u8 *p = my_fifo_get (&usb_txfifo);
+
+	if(p && !myusb_bulk_in_packet (p + 2, p[0] + p[1] * 256)){
+		my_fifo_pop (&usb_txfifo);
 	}
+
 	return 0;
 }
 
 
 //--------------------	UART -------------------------------------------------
-int app_packet_to_uart ()
+int app_data_to_uart ()
 {
 	if (!uart_tx_is_busy ())
 	{
-		//GPIO_WAKEUP_MODULE_HIGH;
 		GPIO_WAKEUP_MODULE_RELEASE;	//release module wake-up signal when TX done
-		u8 *p = my_fifo_get (&uart_fifo);
+		u8 *p = my_fifo_get (&uart_txfifo);
 		if (!p)
 		{
 			return 0;
 		}
-		memcpy(&T_txdata_buf.data, p + 2, p[0]+p[1]*256);
+
+		myuart_data_t T_txdata_buf;
 		T_txdata_buf.len = p[0]+p[1]*256 ;
+		memcpy(&T_txdata_buf.data, p + 2, p[0]+p[1]*256);
 
 		int module_st = gpio_read(GPIO_WAKEUP_MODULE);
 		GPIO_WAKEUP_MODULE_HIGH;
@@ -192,29 +251,23 @@ int app_packet_to_uart ()
 			sleep_us (3000);
 		}
 
-#if( __TL_LIB_8266__ || MCU_CORE_TYPE == MCU_CORE_8266)
+#if (__PROJECT_8266_HOST_DONGLE__)
 		if (uart_Send_kma((u8 *)(&T_txdata_buf)))
 #else
 		if (uart_Send((u8 *)(&T_txdata_buf)))
 #endif
 		{
-			my_fifo_pop (&uart_fifo);
+			my_fifo_pop (&uart_txfifo);
 		}
 	}
 	return 0;
 }
 
-int app_packet_from_uart (void)
+int app_data_from_uart (void)
 {
-	if(rx_uart_w_index==rx_uart_r_index)  //rx buff empty
-		return 0;
-
-	u32 rx_len = T_rxdata_buf[rx_uart_w_index].len + 4 > sizeof(T_rxdata_user) ? sizeof(T_rxdata_user) : T_rxdata_buf[rx_uart_w_index].len + 4;
-	memcpy(&T_rxdata_user, &T_rxdata_buf[rx_uart_w_index], rx_len);
-	//memset(&T_rxdata_buf[rx_uart_w_index],0,sizeof(uart_data_t));
-	if (rx_len > 4)
+	u8 *p = my_fifo_get (&uart_rxfifo);
+	if (p)
 	{
-		u8 *p = T_rxdata_user.data;
 		if (spp_test_en)
 		{
 			if (p[0] == 0xff && p[2] == 0xa0 && p[3] == 0x07)
@@ -238,148 +291,76 @@ int app_packet_from_uart (void)
 
 		if (!spp_test_en || !(p[0] == 0xff && p[2] == 0x1c && p[3] == 0x07 && spp_num != 1))
 		{
-			my_fifo_push (&usb_fifo, T_rxdata_user.data, rx_len - 4);
+			my_fifo_push (&usb_txfifo, &p[4], p[0] + p[1] * 256);
 		}
+		my_fifo_pop (&uart_rxfifo);
 	}
-	rx_uart_w_index = (rx_uart_w_index + 1)&0x01;
 	return 0;
 }
-
-/////////////////////////// led management /////////////////////
-enum{
-	LED_POWER_ON = 0,
-	LED_AUDIO_ON,	//1
-	LED_AUDIO_OFF,	//2
-	LED_SHINE_SLOW, //3
-	LED_SHINE_FAST, //4
-	LED_SHINE_OTA, //5
-};
-
-const led_cfg_t led_cfg[] = {
-	    {1000,    0,      1,      0x00,	 },    //power-on, 1s on
-	    {100,	  0 ,	  0xff,	  0x02,  },    //audio on, long on
-	    {0,	      100 ,   0xff,	  0x02,  },    //audio off, long off
-	    {500,	  500 ,   3,	  0x04,	 },    //1Hz for 3 seconds
-	    {250,	  250 ,   6,	  0x04,  },    //2Hz for 3 seconds
-	    {125,	  125 ,   200,	  0x08,  },    //4Hz for 50 seconds
-};
-
-void app_led_en (int id, int en)
-{
-	id &= 7;
-	en = !(LED_ON_LEVAL ^ en);
-	if (id == 0)
-	{
-		gpio_write(GPIO_LED_RED, en);
-	}
-	else if (id == 1)
-	{
-		gpio_write(GPIO_LED_GREEN, en);
-	}
-	else if (id == 2)
-	{
-		gpio_write(GPIO_LED_BLUE, en);
-	}
-	else if (id == 3)
-	{
-		gpio_write(GPIO_LED_WHITE, en);
-	}
-}
-//////////////////////////////////////////
-// Service Discovery
-//////////////////////////////////////////
-
-
-//////////////////////////////////////////////////////////
-// event call back
-//////////////////////////////////////////////////////////
 
 /////////////////////////////////////////////////////////////////////
 // main loop flow
 /////////////////////////////////////////////////////////////////////
-int main_idle_loop ()
+void main_loop ()
 {
 	static u32 tick_loop;
 	tick_loop ++;
 
-	///////////////////////////////////// proc usb cmd from host /////////////////////
-	extern usb_handle_irq (void);
-	//usb_handle_irq();
+	/// proc uart data ///
+	app_data_to_uart();
+	app_data_from_uart();
 
-	///////////////////////////////////// proc usb cmd from host /////////////////////
-	app_packet_from_uart ();
-	app_packet_to_uart ();
+	/// proc usb data ///
+	app_data_to_usb();
+	app_data_from_usb();
 
-	app_packet_from_usb ();
-	app_packet_to_usb ();
+	//spp_test_write ();
 
-	spp_test_write ();
-
-	///////////////////////////////// bulk out endpoint buffer ready ///////////////////////////////////////
+	/// bulk out endpoint buffer ready ///
 	static u32 tick_bo;
-	if (REG_ADDR8(0x125) & BIT(0))
+	if (REG_ADDR8(0x120 + (USB_ENDPOINT_BULK_OUT & 7)) & BIT(0))
 	{
 		tick_bo = clock_time ();
 	}
 	else if (clock_time_exceed (tick_bo, 200000))
 	{
-		REG_ADDR8(0x125) = BIT(0);
-
+		REG_ADDR8(0x120 + (USB_ENDPOINT_BULK_OUT & 7)) = BIT(0);
 	}
-
-	////////////////////////////////////// UI entry /////////////////////////////////
-	device_led_process();
-
-	dongle_pairing_enable = !gpio_read (SW1_GPIO);
-
-	static u32 gpio2 = 0;
-	u8 gpio =	!gpio_read (SW2_GPIO);
-	if (gpio & !gpio2)
-	{
-		//smp_param_reset ();
-	}
-	gpio2 = gpio;
-	return 0;
 }
-
-void main_loop ()
-{
-	main_idle_loop ();
-}
-
-///////////////////////////////////////////
-//#define	CLK32M_UART115200		uart_Init(19,13,1,1)
 
 void user_init()
 {
-
 	//set UAB ID
 	usb_log_init ();
 	REG_ADDR8(0x74) = 0x53;
 	REG_ADDR16(0x7e) = 0x08d1;
 	REG_ADDR8(0x74) = 0x00;
-	//////////////// config USB ISO IN/OUT interrupt /////////////////
-	//reg_usb_mask = BIT(7);			//audio in interrupt enable
-	//reg_irq_mask |= FLD_IRQ_IRQ4_EN;
-	reg_usb_ep6_buf_addr = 0x80;
-	reg_usb_ep7_buf_addr = 0x60;
 	reg_usb_ep_max_size = (256 >> 3);
-
 	usb_dp_pullup_en (1);  //open USB enum
 
-	/////////////////// keyboard drive/scan line configuration /////////
-	rx_uart_r_index = 0;
-	rx_uart_w_index = 0;
+#if __PROJECT_8266_HOST_DONGLE__
 	gpio_set_func(GPIO_UTX, AS_UART);
 	gpio_set_func(GPIO_URX, AS_UART);
 	gpio_set_input_en(GPIO_UTX, 1);
 	gpio_set_input_en(GPIO_URX, 1);
 	gpio_write (GPIO_UTX, 1);			//pull-high RX to avoid mis-trig by floating signal
 	gpio_write (GPIO_URX, 1);			//pull-high RX to avoid mis-trig by floating signal
-	CLK32M_UART115200;			//todo:change to certain configuration according to lib
-	uart_BuffInit((u8 *)(&T_rxdata_buf), sizeof(T_rxdata_buf), (u8 *)(&T_txdata_buf));
-	REG_ADDR8(0x526) = BIT(0) | BIT(1);//CLR irq source
-	//while (1);
-}
+#else
+	gpio_set_input_en(GPIO_PB2, 1);
+	gpio_set_input_en(GPIO_PB3, 1);
+	gpio_setup_up_down_resistor(GPIO_PB2, PM_PIN_PULLUP_1M);
+	gpio_setup_up_down_resistor(GPIO_PB3, PM_PIN_PULLUP_1M);
+	uart_io_init(UART_GPIO_8267_PB2_PB3);
+#endif
+	reg_dma_rx_rdy0 = FLD_DMA_UART_RX | FLD_DMA_UART_TX; //clear uart rx/tx status
 
+	if(CLOCK_SYS_CLOCK_HZ == 16000000){
+		CLK16M_UART115200;
+	}
+	else if(CLOCK_SYS_CLOCK_HZ == 32000000){
+		CLK32M_UART115200;
+	}
+
+	uart_BuffInit(uart_rxfifo_b, uart_rxfifo.size, uart_txfifo_b);
+}
 #endif
--- ble_lt_sdk/vendor/8266_host_dongle/main.c
+++ ble_lt_sdk/vendor/8266_host_dongle/main.c
@@ -8,25 +8,30 @@
 #include "../../proj_lib/ble/ll/ll.h"
 #include "../../proj_lib/ble/blt_config.h"
 
+
+extern my_fifo_t uart_rxfifo;
 extern void user_init();
-extern void deep_wakeup_proc(void);
-extern uart_data_t T_rxdata_buf[2];
-extern unsigned char rx_uart_r_index;
 _attribute_ram_code_ void irq_handler(void)
 {
-	static	u32	dbg_tx, dbg_rx;
-    unsigned char irqS = uart_IRQSourceGet();
-    if(irqS & BIT(0))	//rx
+	unsigned char irqS = reg_dma_rx_rdy0;
+    if(irqS & FLD_DMA_UART_RX)	//rx
     {
-    	dbg_rx++;
-		rx_uart_r_index = (rx_uart_r_index + 1)&0x01;
-		write_reg16(0x800500,(unsigned short)((unsigned int)(&T_rxdata_buf[rx_uart_r_index])));//set receive buffer address
+    	reg_dma_rx_rdy0 = FLD_DMA_UART_RX;
+    	u8* w = uart_rxfifo.p + (uart_rxfifo.wptr & (uart_rxfifo.num-1)) * uart_rxfifo.size;
+    	if(w[0]!=0)
+    	{
+    		my_fifo_next(&uart_rxfifo);
+    		u8* p = uart_rxfifo.p + (uart_rxfifo.wptr & (uart_rxfifo.num-1)) * uart_rxfifo.size;
+    		reg_dma0_addr = (u16)((u32)p);
+    	}
     }
 
-    if(irqS & BIT(1))	//tx
+    if(irqS & FLD_DMA_UART_TX)	//tx
     {
-    	dbg_tx ++;
-        uart_clr_tx_busy_flag();
+    	reg_dma_rx_rdy0 = FLD_DMA_UART_TX;
+#if __PROJECT_8266_HOST_DONGLE__
+		uart_clr_tx_busy_flag();
+#endif
     }
 }
 
@@ -35,14 +40,11 @@ int main (void) {
 
 	cpu_wakeup_init(CRYSTAL_TYPE);
 
-	set_tick_per_us (32);
+	set_tick_per_us (CLOCK_SYS_CLOCK_HZ/1000000);
+
 	clock_init();
 
 	gpio_init();
-
-	rf_drv_init(CRYSTAL_TYPE);
-
-	//usb_init ();
 
 	user_init ();
 
