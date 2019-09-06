7f90ea45d3c90eb2cf364a7b38b0715eac028b97
sihui.wang <sihui.wang@telink-semi.com>
2019-09-06 16:00:04 +0800

    remove unused project

--- ble_lt_sdk/vendor/common/user_config.h
+++ ble_lt_sdk/vendor/common/user_config.h
@@ -1,14 +1,9 @@
 
 #pragma once
 
-#if (__PROJECT_8266_HOST_DONGLE__)
-	#include "../8266_host_dongle/app_config.h"
-#elif (__PROJECT_8267_MASTER__)
+
+#if (__PROJECT_8267_MASTER__)
 	#include "../8267_master/app_config.h"
-#elif (__PROJECT_8261_BEACON__)
-	#include "../826x_beacon/app_config.h"
-#elif (__PROJECT_8267_BEACON_PAD__)
-	#include "../826x_beacon_pad/app_config.h"
 #elif (__PROJECT_8267_BTUSB__)
 	#include "../8267_btusb/app_config.h"
 #elif (__PROJECT_8261_HCI__|__PROJECT_8267_HCI__|__PROJECT_8269_HCI__)
@@ -17,12 +12,6 @@
 	#include "../826x_hci/8269_app_config.h"
 #elif (__PROJECT_8261_MODULE__ || __PROJECT_8267_MODULE__ || __PROJECT_8269_MODULE__ || __PROJECT_8266_MODULE__)
 	#include "../826x_module/app_config.h"
-#elif (__PROJECT_826X_PROFILE_TEST__)
-	#include "../826x_profile_test/8267_app_config.h"
-#elif (__PROJECT_826X_SPI_MODULE__)
-	#include "../826x_spi_module/8267_app_config.h"
-#elif (__PROJECT_8267_SPI_MASTER__)
-    #include "../8267_spi_master/app_config.h"
 #elif (__PROJECT_DRIVER_TEST__)
 	#include "../8267_driver_test/app_config.h"
 #elif (__PROJECT_8261_BLE_REMOTE__ || __PROJECT_8266_BLE_REMOTE__ || __PROJECT_8267_BLE_REMOTE__ || __PROJECT_8269_BLE_REMOTE__)
@@ -37,18 +26,6 @@
 	#include "../826x_hid_sample/app_config.h"
 #elif (__PROJECT_8266_OTA_BOOT__ || __PROJECT_8261_OTA_BOOT__)
 	#include "../826x_ota_boot/app_config.h"
-#elif (__PROJECT_USB_HOST__)
-	#include "../8267_usb_host/app_config.h"
-#elif (__PROJECT_8266_AUDIO_MIC_SDM__)
-	#include "../8266_ble_audio_mic_sdm/app_config.h"
-#elif (__PROJECT_BEACON_8261__ || __PROJECT_BEACON_8267__)
-	#include "../826x_beacon/app_config.h"
-#elif (__PROJECT_BEACON_DETECTOR_8261__ || __PROJECT_BEACON_DETECTOR_8267__)
-	#include "../826x_beacon_detector/app_config.h"
-#elif (__826x_PTS_TEST__)
-	#include "../826x_pts_test/app_config.h"
-#elif(__PROJECT_5316_BLE_REMOTE__)
-        #include "../531x_ble_remote/app_config.h"
 #else
 	#include "../common/default_config.h"
 #endif
